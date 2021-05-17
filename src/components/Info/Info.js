4243c4aed7897c2f7b3df6771bc1ce280b58c9b7
angel_anna.matthew <angel_anna.mathew@bell.ca>
2021-05-16 23:33:01 -0400

    added responsiveness for tooltip

---
+++ src/components/Info/Info.js
@@ -0,0 +1,40 @@
+import React,{useEffect} from "react";
+import * as styles from "./Info.module.scss";
+import DisplayPicture from "../../components/DisplayPicture/DisplayPicture";
+import Button from "../Button/Button";
+import Timeline from "../Timeline/Timeline";
+import Skillset from "../../components/Skillset/Skillset";
+import Arrow from "../../assets/Arrow.svg";
+import AOS from 'aos';
+import "aos/dist/aos.css";
+
+const Info=()=>{
+    useEffect(() => {
+        AOS.init({
+        duration:'3000',
+        disable:'mobile'
+        });
+        // AOS.refresh();
+      }, []);
+    return(
+        <>
+            <div className={styles.info}  data-aos="fade">
+                <div className={styles.infoSummary}>
+                    <h4>Hi there <div className={styles.wave}>👋</div> ! my name is </h4>
+                    <h1>Angel  Anna Mathew</h1>
+                    <br></br>
+                    <p>
+                    I’m a  Toronto based front-end developer, who is keen on building smooth  UI/UX. I rely on Angular/React to develop responsive performant and accessible web apps. I currently work at Bell Canada. 
+                    </p>
+                    <br></br>
+                    <Button/>
+                </div>
+                <DisplayPicture/>
+            </div>
+            <Skillset/>
+            <Timeline/>
+        </>
+    )
+}
+
+export default Info;
